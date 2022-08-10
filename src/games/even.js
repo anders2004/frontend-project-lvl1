2eaaf853d2cb4e00636d422e6cb04341a60276ce
Anders <anders.yourdev@gmail.com>
2022-08-10 13:41:59 +0300

    moved the game engine to the index.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,35 +1,13 @@
-import readlineSync from 'readline-sync';
-import greeting from '../cli.js';
+import gameEngine from '../index.js';
 import { randomNumber, isEven } from '../lib/lib.js';
 
-const roundsCount = 3;
-let score = 0;
 const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const startGame = () => {
-  const userName = greeting();
-  console.log(gameDescription);
+const gameData = () => {
+  const gameQuestion = randomNumber();
+  const expectedAnswer = isEven(gameQuestion) ? 'yes' : 'no';
 
-  for (let i = 0; i < roundsCount; i += 1) {
-    const number = randomNumber();
-    const expectedAnswer = isEven(number) ? 'yes' : 'no';
-
-    console.log(`Question: ${number}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (expectedAnswer === userAnswer) {
-      console.log('Correct');
-      score += 1;
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-  }
-
-  if (score === roundsCount) {
-    console.log(`Congratulations, ${userName}!`);
-  }
+  return [gameDescription, expectedAnswer];
 };
 
-export default startGame;
+export default () => gameEngine(gameDescription, gameData);
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import readlineSync from 'readline-sync';
+
+const gameEngine = (gameDescription, gameData) => {
+  const gameRounds = 3;
+  let gameScore = 0;
+
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}! \n${gameDescription}`);
+
+  for (let i = 0; i < gameRounds; i += 1) {
+    const [gameQuestion, expectedAnswer] = gameData();
+
+    console.log(`Question: ${gameQuestion}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (expectedAnswer === userAnswer) {
+      console.log('Correct');
+      gameScore += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+
+  if (gameScore === gameRounds) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+
+export default gameEngine;
