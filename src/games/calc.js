017e2427f12ffdb5a9e336bb8a5ea779767da139
Anders <anders.yourdev@gmail.com>
2022-08-25 15:29:27 +0300

    optimized getRandomNumber function, refactored calc.js considering the optimized function

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,28 +5,22 @@ const gameDescription = 'What is the result of the expression?';
 const operators = ['+', '-', '*'];
 
 const calculate = (firstNumber, secondNumber, operator) => {
-  let result = '';
   switch (operator) {
     case '+':
-      result = `${firstNumber + secondNumber}`;
-      break;
+      return `${firstNumber + secondNumber}`;
     case '-':
-      result = `${firstNumber - secondNumber}`;
-      break;
+      return `${firstNumber - secondNumber}`;
     case '*':
-      result = `${firstNumber * secondNumber}`;
-      break;
+      return `${firstNumber * secondNumber}`;
     default:
       throw new Error(`Unknown operator: '${operator}'!`);
   }
-
-  return result;
 };
 
 const getGameData = () => {
   const firstNumber = getRandomNumber(1, 10);
   const secondNumber = getRandomNumber(1, 10);
-  const operator = operators[getRandomNumber(0, operators.length)];
+  const operator = operators[getRandomNumber(0, operators.length - 1)];
   const gameQuestion = `${firstNumber} ${operator} ${secondNumber}`;
   const expectedAnswer = calculate(firstNumber, secondNumber, operator);
   return [gameQuestion, expectedAnswer];
--- src/lib/lib.js
+++ src/lib/lib.js
@@ -1,3 +1,3 @@
-const getRandomNumber = (min = 1, max = 100) => Math.floor(Math.random() * (max - min)) + min;
+const getRandomNumber = (min = 1, max = 100) => Math.floor(Math.random() * (max + 1 - min)) + min;
 
 export default getRandomNumber;
